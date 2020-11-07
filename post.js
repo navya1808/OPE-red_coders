f3e8903c789ec53291c7f363f46dcc52dee8873d
Navya <navyagoyal400@gmail.com>
2020-11-07 15:23:03 +0530

    red

--- post.js
+++ post.js
@@ -11,26 +11,24 @@ function postb(e){
     let textValue=document.getElementById("textValue").value;
     let firstname=document.getElementById("first_name").value;
     let lastname=document.getElementById("last_name").value;
-    if(textValue === "")
+
+    if(firstname != "" && textValue!="")
     {
-        document.getElementById("qww").innerHTML =`<div style="color:red;margin-left:2px;">
-        <small>Fill The Required Field</small>
-        </div>`;   
-    }
-    else{
    document.getElementById("post").innerHTML +=`<div class="card text-center" style="margin:10px 8px;">
     <div class="card-header">
       <b>Post By ~ ${firstname} ${lastname}</b>
     </div>
     <div class="card-body">
       <p class="card-text">${textValue}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <a href="#" class="btn btn-info">Like</a>
     </div>
     <div class="card-footer text-muted">
        <pre><b>Publishing Date :</b> ${date}-${month}-${year}     <b>Publishing Time :</b> ${hours}:${minutes}:${seconds}</pre>
     </div>
   </div>`
     }
+
   form.reset();
   
   }
+ 
